88b8c6fafd510efb580b548063209bf2114eb80f
slimkat <blessings@gmail.com>
2023-02-23 05:08:36 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * checks if letter is capital
+ * @x: the nunber to be checked
+ * Return 1 gor upperletter or 0 for any else
+*/
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return 1;
+	}
+	return 0;
+}
