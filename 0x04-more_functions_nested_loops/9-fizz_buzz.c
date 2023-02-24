7ce0b0efb6cd61c2378891d2370fd55049a9e5f9
slimkat <blessings@gmail.com>
2023-02-23 17:50:12 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-print_triangle.c -o 10-triangles

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,41 @@
-#include "main.h" 
+#include "main.h"
 #include<stdio.h>
 
- /** 
-  *  * main - prints the numbers from 1 to 100, followed by a new line 
-  *  * but for multiples of three prints Fizz instead of the number 
-  *  * and for the multiples of five prints Buzz 
-  *  * Return: Always 0 (Success) 
-*/ 
+ /**
+* main - prints the numbers from 1 to 100, followed by new line
+* but for multiples of three prints Fizz instead of the number
+* and for the multiples of five prints Buzz
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	int i;
-	
-	for (i = 1; i <= 100; i++)
+	int x;
+
+	for (x = 1; x <= 100; x++)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
-		{
-			putchar("Fizz");
-		}
-		else if ((i % 5) == 0 && (i % 3) != 0)
-		{
-			putchar("Buzz");
-		}
-		else if ((i % 3) == 0 && (i % 5) == 0)
-		{
-		putchar(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			putchar("%d", i);
-		}
-		else
-		{
-			putchar(" %d", i);
-		}
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("Fizzbuzz");
 	}
-	putchar("\n");
-	return (0);
+	else if (x % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (x % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d", x);
+	}
+	if (x != 100)
+	{
+		printf(" ");
+	}
+	}
+	printf("\n");
+
+	return (0);
 }
