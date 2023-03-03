ca352c69805bd949138b47437b6f05cc74493b18
slimkat <blessings@gmail.com>
2023-03-02 18:37:09 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -38,21 +38,21 @@
   *       * Return: void 
   *        */ 
  void print_buffer(char *b, int size) 
-	 { 
-		  int i; 
-		   for (i = 0; i <= (size - 1) / 10 && size; i++) 
-			    { 
-				     printf("%08x: ", i * 10); 
-				      if (i < size / 10) 
-					       { 
-						        print_line(b, 9, i); 
-							 } 
-				       else 
-					        { 
-							 print_line(b, size % 10 - 1, i); 
-							  } 
-				        putchar('\n'); 
-					 } 
-		    if (size == 0) 
-			     putchar('\n'); 
-		     }
+{
+	int i; 
+	for (i = 0; i <= (size - 1) / 10 && size; i++) 
+	{ 
+	printf("%08x: ", i * 10); 
+	if (i < size / 10) 
+	{ 
+	print_line(b, 9, i); 
+	} 
+	else 
+	{ 
+	print_line(b, size % 10 - 1, i); 
+	} 
+	_putchar('\n'); 
+	} 
+	if (size == 0) 
+	_putchar('\n'); 
+}
