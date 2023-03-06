da8c300ec9519b14d5dbedec02ce1fa9889b90ca
slimkat <blessings@gmail.com>
2023-03-06 15:05:56 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies nemory area
+ * @dest: where to copy memory to
+ * @n: the number of bytes
+ * @src: the memory to copy from
+ * return: a pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
